2f2975a2af413f85f01cf363eb5e1d349a8bada7
SerSanC <sersanc96@gmail.com>
2020-05-16 16:43:59 +0200

    First Exercise Complete

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -7,17 +7,37 @@ options = ["Piedra", "Papel", "Tijeras"]
 #'Ganaste!'
 #'Perdiste!'
 def quienGana(player, ai):
-    return ""
+    player = player.lower()
+    ai = ai.lower()
+    if(player == "Piedra"):
+        if(ai=="Piedra"):
+            return "Empate!"
+        elif(ai=="Papel"):
+            return "Perdiste!"
+        else: 
+            return "Ganaste!"
+    elif(player == "Papel"):
+        if(ai=="Papel"):
+            return "Empate!"
+        elif(ai=="Tijeras"):
+            return "Perdiste!"
+        else: 
+            return "Ganaste!"
+
+    elif(player == "Tijeras"):
+        if(ai=="Tijeras"):
+            return "Empate!"
+        elif(ai=="Piedra"):
+            return "Perdiste!"
+        else: 
+            return "Ganaste!"
 
 # Entry Point
 def Game():
-    #
-    #
-    
-    #
-    #
-    
+
+    player = input("Elije Piedra, Papel o Tijeras: ")
+    ai =["Piedra","Papel","Tijeras"]
+    ai = random.choice(list(ai))
     winner = quienGana(player, ai)
 
     print(winner)
-
